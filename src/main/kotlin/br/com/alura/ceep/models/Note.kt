334d4286da4ad839b65e0305cbd89098f6bdb068
Alex Felipe <alexfelipevieira@gmail.com>
2022-01-05 10:00:49 -0300

    adicionando suporte para texto grande

--- src/main/kotlin/br/com/alura/ceep/models/Note.kt
+++ src/main/kotlin/br/com/alura/ceep/models/Note.kt
@@ -1,6 +1,7 @@
 package br.com.alura.ceep.models
 
 import java.util.*
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 
@@ -8,7 +9,9 @@ import javax.persistence.Id
 class Note(
     @Id
     val id: UUID = UUID.randomUUID(),
+    @Column(columnDefinition = "TEXT")
     val title: String = "",
+    @Column(columnDefinition = "TEXT")
     val description: String = "",
     val image: String? = null
 )
